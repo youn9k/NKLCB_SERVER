354fb4d376dc606160a6e77fcc4fb45d5ee13ef2
youn9k <ace_lephant@naver.com>
2025-05-04 23:29:24 +0900

    /api 접두사 추가
    추후 리소스와 api를 구분할 수도 있고 url만 봐도 api라는걸 알 수 있게

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
+
+  app.setGlobalPrefix('api')
+
   await app.listen(port);
   console.log(`listening on port ${port}`);
 }
