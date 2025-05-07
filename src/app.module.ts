c333d76969adfa5989a833441691fe0db094f470
youn9k <ace_lephant@naver.com>
2025-05-07 16:55:12 +0900

    TypeORM 및 환경변수 설정
    Sets up TypeORM with asynchronous configuration using environment variables.
    This allows the application to connect to the database based on the current environment (e.g., development, production).
    
    Also configures ConfigModule to manage environment variables using `.env` files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { RecruitModule } from './recruit/recruit.module'
+import { UserEntity } from './user/entity/user.entity';
+import { SocialLoginEntity } from './user/entity/social-login.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, RecruitModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}.local`, `.env.${process.env.NODE_ENV}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get('DB_PORT')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [
+          UserEntity,
+          SocialLoginEntity,
+        ],
+        synchronize: configService.get('DB_SYNC'),
+      }),
+    }),
+    AuthModule, 
+    UserModule,
+    RecruitModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
